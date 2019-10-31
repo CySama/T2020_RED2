0635a7eb0f7864b88ed807d52580e3bfb065327b
Bruce <bruce_gan95@hotmail.com>
2019-10-31 15:09:48 +0800

    COmmit commit

--- frontend/src/Container/mainpage/home.js
+++ frontend/src/Container/mainpage/home.js
@@ -14,80 +14,87 @@ class home extends Component {
   componentDidMount() {
 
     Axios.get("http://127.0.0.1:5000/get_customer_details/marytan")
-    .then(response => {
-        this.setState({customerDetail: response.data});
+      .then(response => {
+        this.setState({ customerDetail: response.data });
         Axios.get("http://127.0.0.1:5000/get_customer_deposit_account/marytan")
-        .then(response => {
-          this.setState({customerAccDetail: response.data[0]});
-          console.log(response.data[0]);
-          this.setState({fetched: true});
-        }).catch(error =>{
-          console.log(error.data);  
-        });
+          .then(response => {
+            this.setState({ customerAccDetail: response.data[0] });
+            console.log(response.data[0]);
+            this.setState({ fetched: true });
+          }).catch(error => {
+            console.log(error.data);
+          });
         console.log(response.data);
       })
       .catch(error => {
         console.log(error.data);
       });
   }
-    render(){
-        return(
-        <React.Fragment>
-          <Container fluid>
-            {this.state.fetched?
+  render() {
+    return (
+      <React.Fragment>
+        <Container fluid>
+          {this.state.fetched ?
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-            <Row>
-              <Col sm={{span:12}} md={{span: 3}}>
-              <LeftSide imageLink={"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"} customerDetail={this.state.customerDetail} customerAccDetail = {this.state.customerAccDetail}/>
-              {/* Hi! {this.state.customerDetail.gender=="Female"? "Ms.": (this.state.customerDetail.gender=="Male"?"Mr.":" ")}{this.state.customerDetail.firstName} {this.state.customerDetail.lastName} */}
-                <Nav variant="pills" className="flex-column">
-                  <Nav.Item>
-                    <Nav.Link eventKey="first">View Transaction</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="second">Expenditure</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="third">Balance</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Col>
-              <Col sm={{span:12}} md={{span: 9}}>
-                <Tab.Content>
-                  <Tab.Pane eventKey="first">
-                    <Chart
-                      width={'500px'}
-                      height={'300px'}
-                      chartType="PieChart"
-                      loader={<div>Loading Chart</div>}
-                      data={[
-                        ['Task', 'Hours per Day'],
-                        ['Work', 11],
-                        ['Eat', 2],
-                        ['Commute', 2],
-                        ['Watch TV', 2],
-                        ['Sleep', 7],
-                      ]}
-                      options={{
-                        title: 'Expenses',
-                        // Just add this option
-                        is3D: true,
-                      }}
-                      rootProps={{ 'data-testid': '2' }}
-                    />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="second">
-
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
-          :null}
-          </Container>
-        </React.Fragment>
-        );
-    }
+              <Row>
+                <Col sm={{ span: 12 }} md={{ span: 3 }}>
+                  <LeftSide imageLink={"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"} customerDetail={this.state.customerDetail} customerAccDetail={this.state.customerAccDetail} />
+                  {/* Hi! {this.state.customerDetail.gender=="Female"? "Ms.": (this.state.customerDetail.gender=="Male"?"Mr.":" ")}{this.state.customerDetail.firstName} {this.state.customerDetail.lastName} */}
+                </Col>
+                <Col sm={{ span: 12 }} md={{ span: 9 }}>
+                  <Chart
+                    width={'500px'}
+                    height={'300px'}
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                      ['Task', 'Hours per Day'],
+                      ['Work', 11],
+                      ['Eat', 2],
+                      ['Commute', 2],
+                      ['Watch TV', 2],
+                      ['Sleep', 7],
+                    ]}
+                    options={{
+                      title: 'Expenses',
+                      // Just add this option
+                      is3D: true,
+                    }}
+                    rootProps={{ 'data-testid': '2' }}
+                  />
+                </Col>
+                <Col sm={{ span: 12 }} md={{ span: 9 }}>
+                  <Chart
+                    width={'500px'}
+                    height={'300px'}
+                    chartType="Histogram"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                      ['Quarks', 'Leptons', 'Gauge Bosons', 'Scalar Bosons'],
+                      [2 / 3, -1, 0, 0],
+                      [2 / 3, -1, 0, null],
+                      [2 / 3, -1, 0, null],
+                      [-1 / 3, 0, 1, null],
+                      [-1 / 3, 0, -1, null],
+                      [-1 / 3, 0, null, null],
+                      [-1 / 3, 0, null, null],
+                    ]}
+                    options={{
+                      title: 'Charges of subatomic particles',
+                      legend: { position: 'top', maxLines: 2 },
+                      colors: ['#5C3292', '#1A8763', '#871B47', '#999999'],
+                      interpolateNulls: false,
+                    }}
+                    rootProps={{ 'data-testid': '5' }}
+                  />
+                </Col>
+              </Row>
+            </Tab.Container>
+            : null}
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default home;
