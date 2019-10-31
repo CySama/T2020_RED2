c74ab2ea6353f5d7990482221f8b23206b959b7b
Bruce <bruce_gan95@hotmail.com>
2019-10-31 13:27:57 +0800

    piechart implemented

--- frontend/src/Container/mainpage/home.js
+++ frontend/src/Container/mainpage/home.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
-import { Row, Tab, Col, Nav, Container} from 'react-bootstrap';
+import { Row, Tab, Col, Nav, Container } from 'react-bootstrap';
 import Axios from 'axios';
 import LeftSide from '../../Components/LeftSide/LeftSide';
-class home extends Component{
+import { Chart } from "react-google-charts";
+class home extends Component {
 
   state = {
     customerDetail: {}
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
 
     Axios.get("http://127.0.0.1:5000/get_customer_details/marytan")
-    .then(response => {
-        this.setState({customerDetail: response.data});
+      .then(response => {
+        this.setState({ customerDetail: response.data });
         console.log(response.data);
-    })
-    .catch(error =>{
+      })
+      .catch(error => {
         console.log(error.data);
-    });
+      });
   }
-    render(){
-        return(
-        <React.Fragment>
-          <Container fluid>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+  render() {
+    return (
+      <React.Fragment>
+        <Container fluid>
+          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
             <Row>
               <Col sm={3}>
-              <LeftSide imageLink={"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"}/>
-              Hi! {this.state.customerDetail.gender=="Female"? "Ms.": (this.state.customerDetail.gender=="Male"?"Mr.":" ")}{this.state.customerDetail.firstName} {this.state.customerDetail.lastName}
+                <LeftSide imageLink={"https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"} />
+                Hi! {this.state.customerDetail.gender == "Female" ? "Ms." : (this.state.customerDetail.gender == "Male" ? "Mr." : " ")}{this.state.customerDetail.firstName} {this.state.customerDetail.lastName}
                 <Nav variant="pills" className="flex-column">
                   <Nav.Item>
                     <Nav.Link eventKey="first">Tab 1</Nav.Link>
@@ -40,19 +41,38 @@ class home extends Component{
               <Col sm={9}>
                 <Tab.Content>
                   <Tab.Pane eventKey="first">
-
+                    <Chart
+                      width={'500px'}
+                      height={'300px'}
+                      chartType="PieChart"
+                      loader={<div>Loading Chart</div>}
+                      data={[
+                        ['Task', 'Hours per Day'],
+                        ['Work', 11],
+                        ['Eat', 2],
+                        ['Commute', 2],
+                        ['Watch TV', 2],
+                        ['Sleep', 7],
+                      ]}
+                      options={{
+                        title: 'Expenses',
+                        // Just add this option
+                        is3D: true,
+                      }}
+                      rootProps={{ 'data-testid': '2' }}
+                    />
                   </Tab.Pane>
                   <Tab.Pane eventKey="second">
-                    
+
                   </Tab.Pane>
                 </Tab.Content>
               </Col>
             </Row>
           </Tab.Container>
-          </Container>
-        </React.Fragment>
-        );
-    }
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default home;
