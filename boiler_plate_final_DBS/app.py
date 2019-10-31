2bc640229ca9bb164daf59050e18dd78b667c1fe
Lenald <lenaldnwj@gmail.com>
2019-10-31 11:44:14 +0800

    Include get_customer_details and get_customer_deposit_account endpoint

--- boiler_plate_final_DBS/app.py
+++ boiler_plate_final_DBS/app.py
@@ -43,7 +43,8 @@ person_schema = PersonSchema()
 persons_schema = PersonSchema(many=True)
 
 
-# Get customer id
+# 3.1 Customers Information
+# GET Customer ID
 @app.route('/get_customer_id/<userName>', methods=['GET'])
 def get_customer_id(userName):
   print(userName)
@@ -55,20 +56,35 @@ def get_customer_id(userName):
   return r
 
 
-# Get Customer Details
-@app.route('/get_customer_details', methods=['GET'])
-def get_customer_details():
-  r = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan', headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
+# GET Customer Details
+@app.route('/get_customer_details/<userName>', methods=['GET'])
+def get_customer_details(userName):
+  url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/' + userName
+  r = requests.get(url, headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
   customer_id = r['customerId']
 
-  url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan'
+  secondary_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/' + customer_id + '/details'
 
-  r_customer_details = requests.get('http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan', headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
+  r_customer_details = requests.get(secondary_url, headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
+  print(r_customer_details)
+  return r_customer_details
 
-  #r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json()
-  #print(r)
-  # return r['bpi']['EUR']['rate']
-  return customer_id
+# 3.3 Accounts
+# GET List of Deposit Accounts
+@app.route('/get_customer_deposit_account/<userName>', methods=['GET'])
+def get_list_deposit_accounts(userName):
+  url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/' + userName
+  r = requests.get(url, headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
+  customer_id = r['customerId']
+
+  secondary_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/accounts/deposit/' + customer_id
+  print(secondary_url)
+  r_customer_deposit_account = requests.get(secondary_url, headers={'identity': 'Group28', 'token': 'cdf48b04-7b42-43a4-a78a-1b781fd3f2d0'} ).json()
+  r_customer_deposit_account = jsonify(r_customer_deposit_account)
+  print(r_customer_deposit_account)
+  return r_customer_deposit_account
+
+
 
 
 # Run Server
