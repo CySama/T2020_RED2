f904b63f3f70afc967b703d58d7820778b67c6bb
Siddharth SIngh <siddharthmdps@gmail.com>
2019-10-31 11:28:40 +0800

    Background implemented

--- frontend/src/Container/Login/Login.js
+++ frontend/src/Container/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Card, Button, Col, Form, Row, InputGroup} from 'react-bootstrap';
+import { Container, Card, Button, Col, Form, Row, InputGroup, Image} from 'react-bootstrap';
 import classes from './Login.module.css'
 import { FaUserAlt, FaLock } from 'react-icons/fa';
 import Background from '../../assets/images/background.jpg';
@@ -14,6 +14,7 @@ class Login extends Component{
     render(){
         return(
             <Container fluid >
+                <Image src="./assets/images/background.jpg" fluid className={classes.Background}/>
                 <Col md={{offset: 9, span:3}} sm={{span: 12}}>
                     <Card>
                     <Card.Img variant="top" src="./assets/images/logo.png" className={classes.Logo}/>
